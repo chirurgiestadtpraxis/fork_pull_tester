b038048f03d0437635821aec805b89142bdba35e
Marlovits Harald <marlovitsh@gmail.com>
2013-12-08 11:08:26 +0100

    and now?

--- ch.marlovits.cdManager/src/ch/marlovits/features/MyClass.java
+++ ch.marlovits.cdManager/src/ch/marlovits/features/MyClass.java
@@ -3,5 +3,6 @@ package ch.marlovits.features;
 public class MyClass {
 	void asdf() {
 		System.out.println("another commit to NewFeatures");
+		System.out.println("how does it look like now???");
 }
 }

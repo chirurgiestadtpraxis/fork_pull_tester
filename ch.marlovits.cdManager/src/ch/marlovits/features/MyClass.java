cbc9df33c4369a81ea91514be3ab44a0256fde0d
Marlovits Harald <marlovitsh@gmail.com>
2013-12-08 10:23:24 +0100

    another commit to NewFeatures

--- ch.marlovits.cdManager/src/ch/marlovits/features/MyClass.java
+++ ch.marlovits.cdManager/src/ch/marlovits/features/MyClass.java
@@ -2,14 +2,6 @@ package ch.marlovits.features;
 
 public class MyClass {
 	void asdf() {
-		System.out.println("ouououou");
-		System.out.println("ouououou");
-		System.out.println("ouououou");
-		System.out.println("ouououou");
-
-		System.out.println("ouououou");
-		System.out.println("ouououou");
-		System.out.println("ouououou");
-		System.out.println("ouououou");
+		System.out.println("another commit to NewFeatures");
 }
 }
